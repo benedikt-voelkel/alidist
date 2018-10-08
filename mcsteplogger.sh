b6743a5c049e2553d6a35823c15df8b6ea024fc1
bvolkel <benedikt.volkel@cern.ch>
2018-10-08 17:58:18 +0200

    Add recipe for MCStepLogger

---
+++ mcsteplogger.sh
@@ -0,0 +1,43 @@
+package: MCStepLogger
+version: "%(tag_basename)s"
+tag: dev
+source: https://github.com/benedikt-voelkel/mcsteplogger
+requires:
+  - Boost
+  - ROOT
+build_requires:
+  - CMake
+---
+#!/bin/bash -e
+
+# Standard ROOT build
+cmake $SOURCEDIR                                                                       \
+      ${CMAKE_GENERATOR:+-G "$CMAKE_GENERATOR"}                                        \
+      -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE                                             \
+      -DCMAKE_INSTALL_PREFIX=$INSTALLROOT                                              \
+
+# Invoke build/install via CMake for standard builds (supports all generators)
+make ${JOBS:+-j$JOBS} && make install
+
+# Modulefile
+mkdir -p etc/modulefiles
+cat > etc/modulefiles/$PKGNAME <<EoF
+#%Module1.0
+proc ModulesHelp { } {
+  global version
+  puts stderr "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
+}
+set version $PKGVERSION-@@PKGREVISION@$PKGHASH@@
+module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
+# Dependencies
+module load BASE/1.0 ${ROOT_VERSION:+ROOT/$ROOT_VERSION-$ROOT_REVISION}     \\
+                     ${BOOST_VERSION:+Boost/$BOOST_VERSION-$BOOST_REVISION}
+# Our environment
+setenv MCSTEPLOGGER_RELEASE \$version
+setenv MCSTEPLOGGER_BASEDIR \$::env(BASEDIR)/$PKGNAME
+prepend-path PATH \$::env(MCSTEPLOGGER_ROOT)/bin
+prepend-path LD_LIBRARY_PATH \$::env(MCSTEPLOGGER_ROOT)/lib
+prepend-path ROOT_INCLUDE_PATH \$::env(MCSTEPLOGGER_ROOT)/include
+$([[ ${ARCHITECTURE:0:3} == osx ]] && echo "prepend-path DYLD_LIBRARY_PATH \$::env(MCSTEPLOGGER_ROOT)/lib")
+EoF
+mkdir -p $INSTALLROOT/etc/modulefiles && rsync -a --delete etc/modulefiles/ $INSTALLROOT/etc/modulefiles
